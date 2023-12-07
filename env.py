32378819d710d4204a566e6e570dfdd6148f7489
Angus Friel <angus.friel.1986@gmail.com>
2023-12-07 11:41:59 +0000

    Push it real good

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+"""
+importing the os module 
+"""
+
+os.environ["DATABASE_URL"] = "postgres://xvxfiitr:j3hrPo5VYgCQHChI8bU2gtSNigEXX8vD@flora.db.elephantsql.com/xvxfiitr"
+os.environ["SECRET_KEY"] = "my_super^secret@key"
